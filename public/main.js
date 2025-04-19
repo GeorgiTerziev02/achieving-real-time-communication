87831c3596e6b1573e8872390d8c23b9bed35da8
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-04-19 16:01:32 +0300

    reactor

--- public/main.js
+++ public/main.js
@@ -1,6 +1,36 @@
 import { RealTimeConnection } from "./real-time-connection.js";
+import { LongPollingTransport } from "./transports/long-polling.js";
+import { ShortPollingTransport } from "./transports/short-polling.js";
+import { SSETransport } from "./transports/sse.js";
 import { WebSocketTransport } from "./transports/websocket.js";
-const transport = new WebSocketTransport();
-const realTimeConnection = new RealTimeConnection(transport);
+var transport = new WebSocketTransport();
+var realTimeConnection = new RealTimeConnection(transport);
 realTimeConnection.start();
+const supportedTransports = [
+    {
+        buttonId: "wsButton",
+        transportFactory: () => new WebSocketTransport()
+    },
+    {
+        buttonId: "sseButton",
+        transportFactory: () => new SSETransport()
+    },
+    {
+        buttonId: "longPollButton",
+        transportFactory: () => new LongPollingTransport()
+    },
+    {
+        buttonId: "shortPollButton",
+        transportFactory: () => new ShortPollingTransport()
+    }
+];
+supportedTransports.forEach(({ buttonId, transportFactory }) => {
+    const button = document.getElementById(buttonId);
+    button.addEventListener("click", () => {
+        realTimeConnection.stop();
+        const transport = transportFactory();
+        realTimeConnection = new RealTimeConnection(transport);
+        realTimeConnection.start();
+    });
+});
 //# sourceMappingURL=main.js.map
