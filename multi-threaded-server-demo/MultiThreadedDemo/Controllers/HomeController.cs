82f9173ac70c0e493ccd4c7e8086bbdb8369bedb
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2025-05-22 23:25:49 +0300

    add multi-threaded server demo

---
+++ multi-threaded-server-demo/MultiThreadedDemo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiThreadedDemo.Controllers
+{
+    public class Test
+    {
+        public int Prop { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // try sending two requests to this endpoint and see what happens
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // freeze the thread for 5 seconds
+            var date = DateTime.Now.AddSeconds(5);
+            while (DateTime.Now < date)
+            {
+                // do nothing
+            }
+            
+            return Ok(new Test
+            {
+                Prop = 1
+            });
+        }
+    }
+}
